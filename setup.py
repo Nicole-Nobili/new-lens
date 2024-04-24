16dbae74ecdc06fab8413e2f97d3b6862c224da1
Nicole <nicole.nobili@mail.polimi.it>
2024-04-23 22:41:21 -0700

    update setup with versions

--- setup.py
+++ setup.py
@@ -5,14 +5,16 @@ setup(
     version='0.0',
     packages=find_packages(),
     install_requires=[ 
-        'numpy',
-        'transformers',
-        'tuned_lens',
-        'torch',
-        'datasets',
-        'sklearn',
+        'numpy==1.26.4',                   
+        'transformers==4.39.1',            
+        'tuned-lens==0.2.0',               
+        'torch==2.1.2+cu121',              
+        'datasets==2.18.0',                
+        'scikit-learn==1.3.2',             
+        'tqdm==4.66.1',                    
     ],
     author='Nicole Nobili',
     author_email='nnobili@ethz.ch',
     license='MIT',
 )
+
